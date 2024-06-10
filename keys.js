5655021b8b3bdeba908eb0da350c7f4a973b4371
Mohammed Malleck <mohammedmalleck@gmail.com>
2024-06-10 11:41:36 +0400

    Refactor(Display Keys): Show the keys that were gitignored previously

---
+++ keys.js
@@ -0,0 +1,6 @@
+export const apiKeys = {
+  rapidAPIKey : '07ab5e45d7msh4a9f1f6662cfd89p105ae4jsn957d1e7dcaee',
+  openWeatherAPIKey : '66e953e8e01f012866b27c8bf0943d97',
+  pixelAPIKey : 'XbFsbTfc5TRPg1y7vLDjhxGzMEGmXy2d1fR0zdN5koD2FVOxPUhZn403',
+  geoCodingAPIKey : 'd3df57e73d0d44c2956e6e2c3da243f5'
+};
